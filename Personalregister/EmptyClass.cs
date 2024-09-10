587731f1e29bc21fe1b59671cd2856b3387bf0ed
Svitlana <rsv0011@gmail.com>
2024-09-10 15:23:19 +0200

    feat: implement the console menu

---
+++ Personalregister/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Personalregister
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static (string Name, decimal Salary)[] workers = new (string, decimal)[100]; 
+    static int workerCount = 0;
+
+    static void Main(string[] args)
+    {
+        ShowMenu(); 
+    }
+
+    static void ShowMenu()
+    {
+        bool isOpen = true;
+
+        while (isOpen)
+        {
+            Console.WriteLine("----- Company Worker Management Menu -----");
+            Console.WriteLine("1. Add Worker");
+            Console.WriteLine("2. Display Workers");
+            Console.WriteLine("3. Exit");
+            Console.Write("Select an option: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    AddWorker();
+                    break;
+                case "2":
+                    DisplayWorkers();
+                    break;
+                case "3":
+                    isOpen = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, please try again.");
+                    break;
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+
+    static void AddWorker()
+    {
+        Console.WriteLine("Not implemented yet");
+    }
+
+   
+    static void DisplayWorkers()
+    {
+         Console.WriteLine("Not implemented yet");
+    }
+}
